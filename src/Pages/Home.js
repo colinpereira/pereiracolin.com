cafc51b958b610577c847cc537aabd8875fd9c8c
colinpereira <64594861+colinpereira@users.noreply.github.com>
2022-02-20 09:00:54 -0800

    initial

---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Styles/Home.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+const Home = () => {
+  return <Container className="homeContainer"></Container>;
+};
+
+export default Home;
